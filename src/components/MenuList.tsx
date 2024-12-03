d9762e078cdc53633d5566174846526e03073846
Marcelo Heinrick Romeiro Senna <marcelohz198@gmail.com>
2024-12-03 10:13:38 -0300

    Patching menu span border not clickable

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -6,11 +6,11 @@ const menuItem = `radius-2 px-4 py-2 rounded-md cursor-pointer menu-item`;
 export default function MenuList() {
     return (
         <ul className="w-full flex justify-evenly menu-list select-none">
-            <li className={menuItem}>
-                <Link href={'/teste'}>Sobre</Link>
+            <li>
+                <Link className={menuItem} href={'/teste'}>Sobre</Link>
             </li>
-            <li className={menuItem}>
-                <Link href={'/teste'}>Portfolio</Link>
+            <li>
+                <Link className={menuItem} href={'/teste'}>Portfolio</Link>
             </li>
         </ul>
     );
