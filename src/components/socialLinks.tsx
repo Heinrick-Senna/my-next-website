d5236f5752b0724802e1ef2fc1e88ad51e36e23d
Marcelo Heinrick Romeiro Senna <marcelohz198@gmail.com>
2024-12-03 10:23:42 -0300

    Inserting social media links

--- src/components/socialLinks.tsx
+++ src/components/socialLinks.tsx
@@ -8,28 +8,28 @@ export default function SocialLinks() {
     return (
         <ul className="flex flex-row justify-center flex-wrap gap-5 md:gap-8 group select-none">
             <li className="group-hover:blur-sm hover:!blur-none hover:scale-125 transition">
-                <a target="__blank" href="https://google.com.br" className={listItem} >
+                <a target="__blank" href="mailto:marcelohz198@gmail.com" className={listItem} >
                     <span>Gmail</span>
                     <MdEmail style={{ transform: 'scale(1.2)', transformOrigin: 'bottom' }} size={24} />
                 </a>
             </li>
 
             <li className="group-hover:blur-sm hover:!blur-none hover:scale-125 transition">
-                <a target="__blank" href="https://google.com.br" className={listItem}>
+                <a target="__blank" href="https://www.linkedin.com/in/marcelo-heinrick/" className={listItem}>
                     <span>Linkedin</span>
                     <FaLinkedin size={24} />
                 </a>
             </li>
 
             <li className="group-hover:blur-sm hover:!blur-none hover:scale-125 transition">
-                <a target="__blank" href="https://google.com.br" className={listItem}>
+                <a target="__blank" href="https://github.com/Heinrick-Senna" className={listItem}>
                     <span>GitHub</span>
                     <FaGithub size={24} />
                 </a>
             </li>
 
             <li className="group-hover:blur-sm hover:!blur-none hover:scale-125 transition">
-                <a target="__blank" href="https://google.com.br" className={listItem}>
+                <a target="__blank" href="https://wa.me/5511967240380" className={listItem}>
                     <span>WhatsApp</span>
                     <IoLogoWhatsapp size={24} />
                 </a>
