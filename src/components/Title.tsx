fab3cead9b9758cf67b7c72d19c7ddeb96ee81fa
Marcelo Heinrick Romeiro Senna <marcelohz198@gmail.com>
2024-12-04 14:12:11 -0300

    Updating Title Style

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,13 +2,13 @@ import './Title.css'
 
 export default function Title() {
   return (
-    <div className='relative select-none'>
+    <div className='relative select-none w-max'>
       <h1 className="absolute text-3xl sm:text-5xl font-bold align-center dissipate-animation">
         Marcelo H R Senna
       </h1>
       <img
         src="./Profile_Pic.jpg"
-        className="absolute right-0 md:right-[-7.4rem] absolute max-w-[220px] top-full translate-y-[calc(-50%+35px)] fade-in-animation"
+        className="absolute right-0 right-1/2 md:right-[-7.4rem] absolute max-w-[220px] top-full translate-y-[calc(-50%+35px)] translate-x-[50%] md:translate-x-0 fade-in-animation"
         alt="Foto de Marcelo H R Senna"
         title="Foto de Marcelo H R Senna"
         style={{
