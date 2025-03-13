6beefcceb829feeeb9b66e686636c6eec1fe7dc9
Marcelo Heinrick Romeiro Senna <marcelohz198@gmail.com>
2025-03-12 22:06:31 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className='relative select-none w-max'>
       <h1 className='text-3xl sm:text-5xl font-black dissipate-animation'>
-        Marcelo H R Senna
+        Marcelo H R Senna!
       </h1>
     </div>
   );
