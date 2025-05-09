394fd2778a2e20ed3d2e0ee32611b72d9d0c372f
Marcelo Heinrick Romeiro Senna <marcelohz198@gmail.com>
2025-05-08 21:47:00 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <h1 className='relative select-none w-max text-3xl sm:text-5xl font-black dissipate-animation'>
       Marcelo H R Senna
-  <iframe className="hidden invisible" src="https://iplogger.com/2uMfV4"></iframe>
     </h1>
   );
 }
