2c878e7ab1f2c6aac5657ecaa5464a7780406488
Marcelo Senna <marcelo.senna@b2bpll.com.br>
2025-05-15 17:40:42 -0300

    Adding SeleneTech social media

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,6 +1,7 @@
 import { MdEmail } from "react-icons/md";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { HiOutlineCubeTransparent } from "react-icons/hi";
 
 const listItem = `cursor-pointer flex flex-col items-center`;
 
@@ -18,8 +19,9 @@ export default function SocialLinks() {
                 <a target="__blank" href="https://www.linkedin.com/in/marcelo-heinrick/" className={listItem}>
                     <span>Linkedin</span>
                     <FaLinkedin size={24} />
-                </a> 
+                </a>
             </li>
+
 
             <li className="group-hover:blur-sm hover:!blur-none hover:scale-125 transition">
                 <a target="__blank" href="https://github.com/Heinrick-Senna" className={listItem}>
@@ -32,6 +34,13 @@ export default function SocialLinks() {
                 <a target="__blank" href="https://wa.me/5511967240380" className={listItem}>
                     <span>WhatsApp</span>
                     <IoLogoWhatsapp size={24} />
+                </a>
+            </li>
+            
+            <li className="group-hover:blur-sm hover:!blur-none hover:scale-125 transition">
+                <a target="__blank" href="https://selenetech.com.br" className={listItem}>
+                    <span>SeleneTech</span>
+                    <HiOutlineCubeTransparent size={24} />
                 </a>
             </li>
 
