07ec18492e427ef0758ce089f0490792be2758d4
Marcelo Senna <marcelo.senna@b2bpll.com.br>
2025-11-17 14:37:37 -0300

    Updating

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,32 +5,72 @@ import Card, { IProjectCard } from "@/components/Card";
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import { use, useEffect, useState } from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 type TSlider = SliderInstance<KeenSliderOptions<{}, {}, KeenSliderHooks>, KeenSliderInstance<{}, {}, KeenSliderHooks>, KeenSliderHooks>
 
 const projects: IProjectCard[] = [
     {
-        name: 'Integração Nasajon',
+        name: 'Integração de ERP para BPO',
         projectType: 'Backend',
-        thumbnail: '../Nasajon.jpg',
+        thumbnail_white: '../PLLWhite.jpg',
+        thumbnail_black: '../PLLBlack.png',
+        description: <>
+            <div>
+                Desenvolvedor principal de uma integração de sistemas entre a maior BPO da América Latina <a className="text-blue-600" target="_blank" href="https://grupopll.com.br">Grupo PLL</a> e o seu,
+                <span className="italic">agora atual,</span> sistema de ERP.
+            </div>
+            <div>
+                Projeto foi desenvolvido em Nest.js e MySQL.
+            </div>
+            <div>
+                A principal funcionalidade da API é o cadastro e atualização de entidades dentro do sistema do ERP em formato de filas, criada e geridas diratemente pelo DB (MySQL), uma das suas funções também
+                é a consulta e reserva de peças que envolve: pedidos de peças, reserva do pedido, criação de uma fração de um tipo de peça, devolução de fração de peça, dentre outras.
+            </div>
+            <div>
+                O objetivo do projeto é facilitar a usabilidade do sistema da BPO para funcionários internos tanto nas operações em laborátorio quanto em operações envolvendo processos fiscais que agora, são feitas com mais facilidade e controle dentro do sistema de ERP.
+            </div>
+        </>,
         projectTags: ['Nodejs', 'Nestjs', 'mySQL', 'Jenkins'],
     },
     {
         name: 'My Personal Website',
         projectType: 'Frontend',
-        thumbnail: '../NextWebsite.webp',
+        thumbnail_black: '../NextWebsite.webp',
+        thumbnail_white: '../NextWebsite.webp',
+        description: <>
+            <div>teste</div>
+        </>,
         projectTags: ['Nodejs', 'NextJs', 'S3', 'Vercel', 'CloudFlare'],
     },
     {
         name: 'Webscrapping App',
         projectType: 'Fullstack',
-        thumbnail: '../Crawler.webp',
+        thumbnail_black: '../Crawler.webp',
+        thumbnail_white: '../Crawler.webp',
+        description: <>
+            <div>teste</div>
+        </>,
         projectTags: ['NodeJS', 'ElectronJS', 'PuppeteerJS'],
     },
     {
         name: 'Micro-Frontend Theme',
         projectType: 'Frontend',
-        thumbnail: '../Microfrontend.png',
+        thumbnail_black: '../Microfrontend.png',
+        thumbnail_white: '../Microfrontend.png',
+        description: <>
+            <div>teste</div>
+        </>,
+        projectTags: ['Nodejs', 'Nestjs', 'mySQL'],
+    },
+    {
+        name: 'Whastapp API',
+        projectType: 'Backend',
+        thumbnail_black: '../Meta.webp',
+        thumbnail_white: '../Meta.webp',
+        description: <>
+            <div>teste</div>
+        </>,
         projectTags: ['Nodejs', 'Nestjs', 'mySQL'],
     }
 ]
@@ -65,12 +105,23 @@ const classPlugin: KeenSliderPlugin = (slider) => {
     slider.on('slideChanged', () => {
         active?.classList.remove('active')
         active = slider.slides[slider.track.details.rel];
+        active.blur();
         active.classList.add('active')
     })
     slider.on('created', () => {
         active?.classList.remove('active')
         active = slider.slides[slider.track.details.rel];
         active.classList.add('active')
+    })
+}
+
+const onClickPlugin: KeenSliderPlugin = (slider) => {
+    slider.on('created', () => {
+        slider.slides.forEach((slide, i) => {
+            slide.addEventListener('click', () => {
+                if (slider.track.details.rel != i) slider.moveToIdx(i);
+            });
+        })
     })
 }
 
@@ -105,8 +156,9 @@ const useWindowSize = () => {
 }
 
 export default function Portfolio() {
-    const [sliderSliderRef, internalSlider] = useKeenSlider<HTMLDivElement>(sliderOptions, [carouselPlugin, classPlugin])
+    const [sliderSliderRef, internalSlider] = useKeenSlider<HTMLDivElement>(sliderOptions, [carouselPlugin, classPlugin, onClickPlugin])
     const size = useWindowSize();
+    const [selectedProject, setSelectedProject] = useState<IProjectCard | null>(null)
 
     useEffect(() => {
         const timeout = setTimeout(() => {
@@ -121,16 +173,58 @@ export default function Portfolio() {
     }, [size]);
 
     useEffect(() => {
-        internalSlider.current?.update({
-            ...sliderOptions
-        });
+        internalSlider.current?.update({ ...sliderOptions });
     }, [internalSlider, sliderOptions]);
 
     return (
-        <div className="scene">
-            <div className="carousel keen-slider" ref={sliderSliderRef}>
-                {projects.map((project, i) => <Card key={i} data={project} />)}
+        <>
+            <div className="scene">
+                <div className="carousel keen-slider" ref={sliderSliderRef}>
+                    {projects.map((project, i) => (
+                        <div
+                            onClick={() => internalSlider.current?.track.details.rel == i ? setSelectedProject(project) : null}
+                            onKeyUp={(e) => {
+                                if (e.key != 'Enter') return;
+
+                                if (i == internalSlider.current?.track.details.rel) return setSelectedProject(project);
+
+                                return internalSlider.current?.moveToIdx(i);
+                            }}
+                            tabIndex={
+                                i == internalSlider.current?.track.details.rel
+                                    || i == internalSlider.current?.track.absToRel(internalSlider.current?.track.details.rel - 1)
+                                    || i == internalSlider.current?.track.absToRel(internalSlider.current?.track.details.rel + 1)
+                                    ? 0 : undefined
+                            }
+                            className="relative rounded-md bg-black flex flex-col border-current border-2 justify-start cursor-pointer carousel__cell">
+                            <Card key={i} data={project} />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+
+            <Dialog
+                modal={true}
+                open={!!selectedProject}
+                onOpenChange={(open) => !open && setSelectedProject(null)}
+            >
+                <DialogContent className="max-w-xs sm:max-w-screen-xl max-h-[80vh] overflow-auto dialog-content-animated">
+                    <DialogHeader className="flex flex-col gap-2">
+                        <DialogTitle>{selectedProject?.name} - {selectedProject?.projectType}</DialogTitle>
+                        <div className="flex flex-wrap gap-2">
+                            {selectedProject?.projectTags.map((tag, i) => (
+                                <span key={i} className="bg-muted px-2 py-1 rounded text-xs border-current border-2">{tag}</span>
+                            ))}
+                        </div>
+                    </DialogHeader>
+                    <div className="space-y-2">
+                        <div className="px-2 flex flex-col gap-6">
+                            {selectedProject?.description}
+                        </div>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </>
+
     )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,37 +1,50 @@
+import { ReactElement } from "react";
+
 export interface IProjectCard {
     name: string;
     projectType: 'Frontend' | 'Backend' | 'Fullstack';
-    thumbnail: string;
+    description: ReactElement;
+    thumbnail_black: string;
+    thumbnail_white: string;
     projectTags: string[];
 }
 
 export default function Card({ data }: { data: IProjectCard }) {
     return (
-        <div className="relative rounded-md bg-black flex flex-col border-current border-2 p-3 gap-2 justify-start cursor-pointer carousel__cell">
-            
-            <div 
-                className="absolute w-full h-full left-0 top-0 z-[-1]"
+        <>
+
+            <div className="absolute flex flex-wrap justify-between items-center top-0 translate-y-[calc(-100%-.75rem)] w-full title-span">
+                <span className="font-extrabold text-xl h-fit">{data.name}</span>
+                <span className="p-2 rounded-md font-bold border-2 border-current">{data.projectType}</span>
+            </div>
+
+            <div
+                className="thumb-white absolute w-full h-full left-0 top-0 z-[-1] transition-opacity duration-[400ms]"
                 aria-label={`Project thumbnail for ${data.name}`}
                 style={{
-                    backgroundImage: `url(${data.thumbnail})`,
+                    backgroundImage: `url(${data.thumbnail_white})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            />
+            <div
+                className="thumb-black absolute w-full h-full left-0 top-0 z-[-1] transition-opacity duration-[400ms]"
+                aria-label={`Project thumbnail for ${data.name}`}
+                style={{
+                    backgroundImage: `url(${data.thumbnail_black})`,
                     backgroundRepeat: 'no-repeat',
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                 }}
             />
 
-
-            <div className="flex flex-wrap justify-between items-center">
-                <span className="p-2 rounded-md font-extrabold text-xl bg-backgroundOpacity/65 text-center h-fit">{data.name}</span>
-                <span className="p-2 rounded-md font-bold bg-backgroundOpacity/65 border-2 border-current">{data.projectType}</span>
-            </div>
-
-            <div className="flex flex-wrap gap-4 text-xs font-bold h-auto items-start">
+            <div className="flex flex-wrap gap-4 text-xs font-bold h-auto items-start p-4">
                 {
-                    data.projectTags.map((tag, i) => <span key={i} className="bg-foreground text-background py-1 px-2 rounded-md">{tag}</span>)
+                    data.projectTags.slice(0, 4).map((tag, i) => <span key={i} className="bg-foreground text-background p-2 rounded-md">{tag}</span>)
                 }
             </div>
 
-        </div >
+        </>
     )
 }
