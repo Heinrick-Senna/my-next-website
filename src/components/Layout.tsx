c29349d5154e40cac9a567a2a3b3d2e838ecb411
Heinrick Senna <marcelohsenna@protonmail.com>
2026-05-25 22:39:31 -0300

    Updating menu layout & adding blog page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,9 +15,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
                 <div className="relative grid grid-rows-[1fr_auto] items-center justify-items-center min-h-screen p-12 md:p-8">
                     <main className="lg:absolute flex flex-col gap-8 items-center sm:items-start z-10 mb-12 lg:mb-0 max-w-[calc(100%-2rem)] md:max-w-[50vw]">
                         {children}
-                        <MenuList />
                     </main>
-                    <footer className="flex gap-6 flex-wrap items-center justify-center self-end relative z-10">
+                    <footer className="flex sm:gap-20 gap-6 flex-wrap items-center justify-center self-end relative z-10">
+                        <MenuList />
                         <SocialLinks />
                     </footer>
                 </div>
--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -5,12 +5,15 @@ const menuItem = `radius-2 px-4 py-2 font-semibold rounded-md border-2 border-cu
 
 export default function MenuList() {
     return (
-        <ul className="w-full flex justify-evenly menu-list select-none min-w-[15vw]">
+        <ul className="w-full gap-y-4 flex justify-evenly select-none min-w-[15vw] flex-col menu-list sm:flex-row">
             <Link href={'/about'} className={menuItem} prefetch={true}>
                 <span>Sobre</span>
             </Link>
             <Link href={'portfolio'} className={menuItem} prefetch={true}>
                 <span>Portfolio</span>
+            </Link>
+            <Link href={'/blog'} className={menuItem} prefetch={true}>
+                <span>Blog</span>
             </Link>
         </ul>
     );
